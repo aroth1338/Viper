67380a737c34b55a59d9a483fd09dfe9ea919c94
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2023-05-17 10:55:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ import ast
 #Only change __version__ in data_visualization/__init__.py file
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('vipervis/__init__.py', 'rb') as f:
+with open('viper/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
 setup(
     # Needed to silence warnings 
-    name='ViperVis',
+    name='Viper',
     url='https://github.com/aroth1338/ViperViz',
-    download_url = 'https://github.com/aroth1338/ViperViz',
+    download_url = 'https://github.com/aroth1338/Viper',
     author='Adam Roth',
     author_email='aroth1338@gmail.com',
     # Needed to actually package something
-    packages=['viperviz', 'viperviz.ColorWheel', 'viperviz.icons'],
+    packages=['viper', 'viper.ColorWheel', 'viper.icons'],
     # Needed for dependencies
     install_requires=['numpy', 'matplotlib >= 3.5.0', 'pillow'],
     # *strongly* suggested for sharing
