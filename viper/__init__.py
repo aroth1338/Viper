82f3497b1744d4565732677122a0dcc82e624e5e
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2023-05-17 10:45:48 -0400

    renamed viperviz to viper

--- viper/__init__.py
+++ viper/__init__.py
@@ -1,9 +1,9 @@
 __version__ = 0.1.0
 
-import viperviz.ColorWheel
-import viperviz.icons
-import viperviz.plot_annotations
-import viperviz.main_plotting
+import viper.ColorWheel
+import viper.icons
+import viper.plot_annotations
+import viper.main_plotting
 
 import matplotlib as mpl
 mpl.rcParams["axes.facecolor"]   = "none"
