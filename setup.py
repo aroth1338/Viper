b836eb60be2450c6e4528614e07b34a79b630b7a
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2023-05-25 09:12:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,19 +10,15 @@ with open('viper/__init__.py', 'rb') as f:
         f.read().decode('utf-8')).group(1)))
 
 setup(
-    # Needed to silence warnings 
     name='Viper',
     url='https://github.com/aroth1338/ViperViz',
     download_url = 'https://github.com/aroth1338/Viper',
     author='Adam Roth',
     author_email='aroth1338@gmail.com',
-    # Needed to actually package something
     packages=['viper', 'viper.ColorWheel', 'viper.icons', "viper.main_plotting", "viper.plot_annotations"],
-    # Needed for dependencies
+    #dependencies
     install_requires=['numpy', 'matplotlib >= 3.5.0', 'pillow'],
-    # *strongly* suggested for sharing
     version=version,
-    # The license can be anything you like
     license='MIT',
     description='Supplemental package to assist with data visualization in Python.',
     long_description=open('README.md').read(),
