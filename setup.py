96c4bce18f823dd0a7bb57f94239f2603d4e403b
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2023-08-24 14:33:27 -0400

    added color_list

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import re
 import ast
 
-#Only change __version__ in data_visualization/__init__.py file
+#Only change __version__ in viper/__init__.py file
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('viper/__init__.py', 'rb') as f:
@@ -15,7 +15,8 @@ setup(
     download_url = 'https://github.com/aroth1338/Viper',
     author='Adam Roth',
     author_email='aroth1338@gmail.com',
-    packages=['viper', 'viper.ColorWheel', 'viper.icons', "viper.main_plotting", "viper.plot_annotations"],
+    packages=['viper', 'viper.ColorWheel', 'viper.ColorWheel.color_list.txt', 'viper.icons', "viper.main_plotting", "viper.plot_annotations"],
+    package_data={'viper': ['viper/ColorWheel/*.txt']},
     #dependencies
     install_requires=['numpy', 'matplotlib >= 3.5.0', 'pillow'],
     version=version,
